f2a349b6623292b5c8040bd2e8170b7bad672222
Branimir <brani_svetoslavov@abv.bg>
2016-09-19 20:35:12 +0300

    Added C# HQC-2 homework 1

---
+++ JavaScript-Applications-Homeworks/01. Promises and asynchronous programming/Task-One/script.js
@@ -0,0 +1,44 @@
+(function() {
+    let locationElement = document.getElementById("location-wrapper");
+
+    function getGeolocationPositionPromise() {
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    resolve(position);
+                },
+                (error) => {
+                    reject(error);
+                });
+        });
+    }
+
+    function parseLatAndLongCoords(geolocationPosition) {
+        if (geolocationPosition.coords) {
+            return {
+                lat: geolocationPosition.coords.latitude,
+                long: geolocationPosition.coords.longitude
+            };
+        } else {
+            throw {
+                message: "No coords element found",
+                name: "UserException"
+            };
+        }
+    }
+
+    function createGeolocationImage(coordsObj) {
+        let imgElement = document.createElement("img"),
+            imgSrc = "http://maps.googleapis.com/maps/api/staticmap?center=" +
+            coordsObj.lat + "," +
+            coordsObj.long + "&zoom=17&size=500x500&sensor=false";
+
+        imgElement.setAttribute("src", imgSrc);
+
+        locationElement.appendChild(imgElement);
+    }
+
+    getGeolocationPositionPromise()
+        .then(parseLatAndLongCoords)
+        .then(createGeolocationImage);
+}());
